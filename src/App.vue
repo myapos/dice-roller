b3aacfdc3737eeb71cca5ef6ce7b039af1ed95b0
Myron Apostolakis <myapos@yahoo.com>
2024-11-16 22:46:44 +0000

    added footer in app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <DiceRoller />
+  <main>
+
+    <DiceRoller />
+    <footer>Created by <a href="https://github.com/myapos" target="_blank">Myron Apostolakis</a></footer>
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -7,3 +11,19 @@ import DiceRoller from './components/DiceRoller.vue';
 // This question is adapted from FrontendEval's Rolling Dice question.
 // link: https://frontendeval.com/questions/rolling-dice
 </script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  height: 95vh;
+}
+
+footer {
+  text-align: center;
+  font-size: 0.8rem;
+  color: grey;
+}
+</style>
